c0bf2c93b838da2d69f61acb4640878c2f12ba81
RoskiDeluge <roberto.delgado@gmail.com>
2022-07-26 12:42:19 -1000

    Refactored to make more legible

--- csv_clean.py
+++ csv_clean.py
@@ -12,14 +12,15 @@ with open(inFile,'r') as i:
 
 df = DataFrame(dataframe)
 
-def remove_others(df: DataFrame, columns: Set[Any]):
+def remove_columns(df: DataFrame, columns: Set[Any]):
     cols_total: Set[Any] = set(df.columns)
     diff: Set[Any] = cols_total - columns
     df.drop(diff, axis=1, inplace=True)
 
 
 # Keep columns specified in 2nd argument set
-remove_others(df, {"email", "first_name"})
+columns_to_keep = {"email", "first name", "last name", "zipcode", "userstate", "usercity", "ethnicity", "email_source", "email_preference", "cellphone", "sms_status", "permission_status", "parental_status", "utm_source", "messenger user id", "advocacy_feed", "advocacy", "parenting_feed", "parenting_engagement", "signed up", "gender", "timezone"}
+remove_columns(df, columns_to_keep)
 
 file = "editedfile.csv"
 
