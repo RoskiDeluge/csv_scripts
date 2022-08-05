bee1024cd58f9dca5c15df11e13d9c4057d7ecd3
RoskiDeluge <roberto.delgado@gmail.com>
2022-08-05 06:39:45 -1000

    Added documentation

--- csv_clean.py
+++ csv_clean.py
@@ -1,30 +1,45 @@
 import sys
 import pandas as pd
+from uuid import uuid4
 from pandas import DataFrame
 from typing import Set, Any
 
+# Get file name from command line
 inFile = sys.argv[1]
-# outFile = sys.argv[2]
 
-
-with open(inFile,'r') as i:
+# Open file
+with open(inFile, 'r') as i:
+    # Create dataframe
     dataframe = pd.read_csv(i)
 
 df = DataFrame(dataframe)
 
+
 def remove_columns(df: DataFrame, columns: Set[Any]):
+    """
+    Remove columns from a DataFrame
+
+    :param df: DataFrame
+    :param columns: Set of columns to keep
+    :return: DataFrame
+    """
     cols_total: Set[Any] = set(df.columns)
     diff: Set[Any] = cols_total - columns
     df.drop(diff, axis=1, inplace=True)
 
 
-# Keep columns specified in 2nd argument set
-columns_to_keep = {"email", "first name", "last name", "zipcode", "userstate", "usercity", "ethnicity", "email_source", "email_preference", "cellphone", "sms_status", "permission_status", "parental_status", "utm_source", "messenger user id", "advocacy_feed", "advocacy", "parenting_feed", "parenting_engagement", "signed up", "gender", "timezone"}
+# Specify columns to keep
+columns_to_keep = {"email", "first name", "last name", "zipcode", "userstate", "usercity", "ethnicity", "email_source", "email_preference", "cellphone", "sms_status",
+                   "permission_status", "parental_status", "utm_source", "messenger user id", "advocacy_feed", "advocacy", "parenting_feed", "parenting_engagement", "signed up", "gender", "timezone"}
+
 remove_columns(df, columns_to_keep)
 
-file = "csvc_editedfile.csv"
+# Create new file name
+new_id = str(uuid4())
+newcsv = f'{new_id}.csv'
 
-df.to_csv(file, index=False, encoding='utf-8')
+# Write new file
+df.to_csv(newcsv, index=False, encoding='utf-8')
 
 
 """
